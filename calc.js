b65f0eb42d2e0d921ba649bb9f43649819fa2148
sakhiul_alam <sakhiulalam67@gmail.com>
2024-03-10 10:17:18 +0600

    some modified style

--- calc.js
+++ calc.js
@@ -1,14 +1,13 @@
-function button(number){
-    var results = document.getElementById("result");
-    results.value+=number;
+function button(number) {
+  var results = document.getElementById("result");
+  results.value += number;
 }
-function calculation(){
-    var calculation = document.getElementById("result").value;
-    var fResult = eval(calculation);
-    document.getElementById("result").value=fResult;
-
+function calculation() {
+  var calculation = document.getElementById("result").value;
+  var fResult = eval(calculation);
+  document.getElementById("result").value = fResult;
 }
 
-function clr(){
-    document.getElementById("result").value="";
+function clr() {
+  document.getElementById("result").value = "";
 }
